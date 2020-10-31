d5201334adf81751906f59a4a2f7dd2cfbb0929a
Viet Anh <vietda@vnpt.vn>
2020-10-31 21:14:40 +0700

    t

--- js/app.js
+++ js/app.js
@@ -63,3 +63,16 @@ button.onclick = function() {
   canvas.height = video.videoHeight;
   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 };
+
+function onScanSuccess(qrCodeMessage) {
+  // handle on success condition with the decoded message
+  console.log("da doc dc qrcode");
+}
+
+function onScanError(errorMessage) {
+  // handle on error condition, with error message
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess, onScanError);
