9701d2f2127423235f07e9f78f433b243ac43fd8
Viet Anh <vietda@vnpt.vn>
2020-10-31 20:16:08 +0700

    xxx

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 window.onload = () => {
   'use strict';
 
@@ -6,3 +8,26 @@ window.onload = () => {
              .register('./serviceWorker.js');
   }
 }
+
+var constraints = {
+  video: true
+};
+
+var video = document.querySelector('video');
+
+function handleSuccess(stream) {
+  window.stream = stream; // only to make stream available to console
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('getUserMedia error: ', error);
+}
+
+//navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+navigator.getUserMedia = (navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia ||
+    false);
+navigator.getUserMedia({ video: true, audio: false }, handleSuccess, handleError);
